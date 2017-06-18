4f562ed4339eeb8867b6da395b3a6ed9c66fb02a
Rovashan <rovashan.subbrayan@sgits.co.za>
2017-06-18 19:01:50 +0200

    Start Q2 - call gMap web service

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,14 @@
 package controllers;
 
+import javax.inject.Inject;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import play.core.j.HttpExecutionContext;
 import play.mvc.*;
+import play.libs.ws.*;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 import views.html.*;
 
@@ -16,6 +24,9 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
+    @Inject WSClient ws;
+    //@Inject HttpExecutionContext ec;
+
     public Result index() {
         return ok(index.render("Hello world"));
     }
@@ -25,4 +36,19 @@ public class HomeController extends Controller {
         return ok(Integer.toString(total));
     }
 
+    public CompletionStage<Result> suggestcat(int code) {
+        String wsUrl = "https://maps.googleapis.com/maps/api/geocode/json?region=za&address=" + code;
+
+        WSRequest request = ws.url(wsUrl);
+        //CompletionStage<WSResponse> responsePromise = request.get();
+
+        //CompletionStage<JsonNode> jsonPromise = ws.url(wsUrl).get()
+        //       .thenApply(WSResponse::asJson);
+
+        //String result = jsonPromise.toString();
+
+        return request.get().thenApply(response ->
+                ok("Feed title: " + response.asJson())
+        );
+    }
 }
